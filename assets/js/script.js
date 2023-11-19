c31b4e6ac200ff57fb0b351c9b866c0c99ecb3df
Greg Duke <105805424+SatsumaSegment@users.noreply.github.com>
2023-11-19 12:32:06 +0000

    Added a function to get latitude and longitude based on user's search and a function to get weather data based on the lat and lon.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,40 @@
 
 const API = "1a711f07d39b42a9b9a418f8a796c5b0"; // API Key
 
-var userSearch = $('#search-input');
-var searchButton = $('#search-button');
+var userSearch = $('#search-input');    // User's search input
+var searchButton = $('#search-button'); // Submit button for search form
 
+var coords = []; // Array to store lat and lon coordinates
 
+// Function to get lat and lon based on user's search
+function getCoords(event) {
+    event.preventDefault(); // Prevent form from refeshing page
+    var location = userSearch.val(); // Store user's search
+    var url = `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=5&appid=${API}` // Geo API for getting lon and lat given user's search
 
-function getCoords(e) {
-    e.preventDefault();
-    var location = userSearch.val();
-    var url = `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=5&appid=${API}` // Geo API for getting lon and lat
-
-    var coords = [];
-
+    // Fetch request from API to get coodinates and store in coords array
     fetch(url).then(function(response) {
         return response.json();
     }).then(function(data) {
         coords.push(data[0].lat);
-        coords.push(data[0].lon);
+        coords.push(data[1].lon);
+    }).then(getWeatherData(coords)); // Call the getWeatherData function passing the coods array
+}
+
+// Function to fetch weather data
+function getWeatherData(coords) {
+    // Round lat and lon coordinates to 2 decimal places
+    var lat = Math.floor(coords[0] * 100) / 100;
+    var lon = Math.floor(coords[1] * 100) / 100;
+    var url = "api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API}"; // Pass lat and lon to the forcast API
+
+    // Fetch weather data from API.
+    fetch(url).then(function(response) {
+        return response.json();
+    }).then(function(data) {
+        console.log(data);
         return;
     });
-
-    return coords;
 }
 
 searchButton.on('click', getCoords);
